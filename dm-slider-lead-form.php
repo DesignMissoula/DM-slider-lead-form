ab391d3648065c700ad9687becbad638834b8025
Bradford Knowlton <brad@ucanbonline.net>
2017-08-27 14:58:59 -0600

    added contact button

--- dm-slider-lead-form.php
+++ dm-slider-lead-form.php
@@ -5,7 +5,7 @@ Plugin Name: DM Slider Lead Form
 Plugin URI: http://www.designmissoula.com/
 Description: This is not just a plugin, it makes WordPress better.
 Author: Bradford Knowlton
-Version: 1.0.2
+Version: 1.0.3
 Author URI: http://bradknowlton.com/
 GitHub Plugin URI: https://github.com/DesignMissoula/DM-clients
 */
@@ -15,17 +15,20 @@ GitHub Plugin URI: https://github.com/DesignMissoula/DM-clients
 Change Log
 
 1.0.2 Initial version
+1.0.3 Added Contact Button
 
 */
 
 
 function dm_enqueue_script() {   
-    wp_enqueue_script( 'tabSlideOut', plugin_dir_url( __FILE__ ) . 'js/jquery.tabSlideOut.v1.3.js', array( 'jquery' ), '1.0.2', true );
+    wp_enqueue_script( 'tabSlideOut', plugin_dir_url( __FILE__ ) . 'js/jquery.tabSlideOut.v1.3.js', array( 'jquery' ), '1.0.3', true );
 }
 add_action('wp_enqueue_scripts', 'dm_enqueue_script');
 
 
 function dm_footer_function() {
+	
+	echo "<a class='handle' href='#' style='background-image: url(&quot;".plugin_dir_url( __FILE__ )."img/contact_tab.gif&quot;); background-repeat: no-repeat; width: 40px; height: 122px; text-indent: -99999px; display: block; outline: medium none; position: absolute; top: 0px; right: -40px;'>Content</a>"
 	
 	echo "<div class='slide-out-div'>
             <a class='handle' href='http://link-for-non-js-users.html'>Content</a>
